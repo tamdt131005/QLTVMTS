cce0bdb1d59fa9885ae1835dc712980f71740556
tamdt131005 <tamdt.a11k48gtb@gmail.com>
2024-12-10 21:27:36 +0700

    add mdi

---
+++ QLMT/MDIfrm.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace QLMT
+{
+    partial class MDIfrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.hệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.đăngXuấtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýDanhMụcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hệThốngToolStripMenuItem,
+            this.quảnLýDanhMụcToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // hệThốngToolStripMenuItem
+            // 
+            this.hệThốngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.đăngXuấtToolStripMenuItem});
+            this.hệThốngToolStripMenuItem.Name = "hệThốngToolStripMenuItem";
+            this.hệThốngToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.hệThốngToolStripMenuItem.Text = "Hệ Thống";
+            // 
+            // đăngXuấtToolStripMenuItem
+            // 
+            this.đăngXuấtToolStripMenuItem.Name = "đăngXuấtToolStripMenuItem";
+            this.đăngXuấtToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.đăngXuấtToolStripMenuItem.Text = "Đăng Xuất";
+            // 
+            // quảnLýDanhMụcToolStripMenuItem
+            // 
+            this.quảnLýDanhMụcToolStripMenuItem.Name = "quảnLýDanhMụcToolStripMenuItem";
+            this.quảnLýDanhMụcToolStripMenuItem.Size = new System.Drawing.Size(146, 24);
+            this.quảnLýDanhMụcToolStripMenuItem.Text = "Quản Lý Danh Mục";
+            // 
+            // MDIfrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MDIfrm";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hệThốngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem đăngXuấtToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýDanhMụcToolStripMenuItem;
+    }
+}
+
---
+++ QLMT/Public.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLHSSV
+{
+    internal class Public
+    {
+        public static SqlConnection conn;
+        public static SqlCommand cmd;
+        static string sql;
+
+
+        public static SqlConnection KetNoi()
+        {
+            string strConn = @"Data Source=.;Initial Catalog=QLSV23;Integrated Security=True;Encrypt=False";
+            conn = new SqlConnection(strConn);
+            return conn;
+        }
+
+        public static DataTable LayDuLieu(string sql)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(sql, KetNoi());
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+  
+        public static void GanNguonDataGridView(DataGridView dgDanhSach, string sql)
+        {
+            dgDanhSach.DataSource = LayDuLieu(sql);
+        }
+        public static void GanNguonComboBox(ComboBox cboName, string TableName, string DisplayField, string KeyField)
+        {
+            sql = $"Select {KeyField},{DisplayField} From {TableName}";
+            cboName.DataSource = LayDuLieu(sql);
+            cboName.DisplayMember = DisplayField;
+            cboName.ValueMember = KeyField;
+        }
+        public static bool XoaHang(DataGridView dtdanhsach, string bang, string cot, string dieukien)
+        {
+            string lenhxoa = "DELETE FROM " + bang + " WHERE " + cot + " = '" + dieukien + "'";
+            try
+            {
+                cmd = new SqlCommand(lenhxoa, KetNoi());
+                if (conn.State != ConnectionState.Open)
+                {
+                    conn.Open();
+                }
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex) { return false; }
+        }
+            
+        public static bool ktTrungMa(string FieldName, string Table, bool ktThem,string ValueNew, string ValueOld)
+        {
+            if (ktThem == true)
+                sql = "Select " + FieldName + " From " + Table + " Where " +FieldName + " = '" + ValueNew + "'";
+            else
+                sql = "Select " + FieldName + " From " + Table + " Where " +FieldName + " = '" + ValueNew + "' and " + FieldName + " <> '" + ValueOld + "'";
+            DataTable dt = LayDuLieu(sql);
+            if (dt.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
+    
+
